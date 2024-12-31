cae172bbd1f78b580568455c9e966b5c05c9e228
Daniel liu <liudaniel0322@gmail.com>
2024-12-31 10:18:36 -0500

    test

--- User.js
+++ User.js
@@ -1,12 +1,4 @@
-async function getProfile(accessToken) {
-    let accessToken = localStorage.getItem('access_token');
-  
-    const response = await fetch('https://api.spotify.com/v1/me', {
-      headers: {
-        Authorization: 'Bearer ' + accessToken
-      }
-    });
-  
-    const data = await response.json();
-  }
-  
+import puppeteer from 'puppeteer-core'// Or import puppeteer from 'puppeteer-core';
+
+// Launch the browser and open a new blank page
+const browser = await puppeteer.launch();
