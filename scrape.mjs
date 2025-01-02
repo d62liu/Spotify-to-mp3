6dab14ebe3949bfe56d81137cb0c76a064656aa4
Daniel liu <liudaniel0322@gmail.com>
2025-01-02 16:31:04 -0500

    Update scrape.mjs

--- scrape.mjs
+++ scrape.mjs
@@ -1,5 +1,7 @@
 import puppeteer from 'puppeteer';
+import * as process from "process.js";
 
+const a = process.
 const browser = await puppeteer.launch({
     headless: false,
     executablePath: 'C:/Program Files/Google/Chrome/Application/chrome.exe',
@@ -8,6 +10,7 @@ const page = await browser.newPage();
 await page.goto('https://www.youtube.com/');
 await page.type('[name="search_query"]', 'Reminiscence by Yuji Nomi');
 await page.keyboard.press('Enter');
+
 
 
 
