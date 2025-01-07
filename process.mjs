0a721482fc3226db2017bc87ed6a30a23999024b
Daniel liu <liudaniel0322@gmail.com>
2025-01-07 12:12:47 -0500

    Update process.mjs

--- process.mjs
+++ process.mjs
@@ -1,6 +1,6 @@
-import ytdl from "@distube/ytdl-core";
+import ytdl from '@distube/ytdl-core'
 import fs from "fs";
-import { main } from "./index.mjs";
+import {main} from "./index.mjs" 
 const url = "https://www.youtube.com/watch?v=asBlT_zETzU&ab_channel=supertf";
 
 main();
