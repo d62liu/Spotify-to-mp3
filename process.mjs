59ae5c535994f670370a371af3a6ca51b2224326
Daniel liu <liudaniel0322@gmail.com>
2025-01-08 19:23:12 -0500

    implemented concurrency

--- process.mjs
+++ process.mjs
@@ -1,27 +1,46 @@
 import ytdl from '@distube/ytdl-core'
 import fs from "fs";
 import {main, getPlaylistItems} from "./index.mjs" 
+import pLimit from 'p-limit'; 
 
-// const folderPath = prompt("..");
 const url = await main();
 const playlist = await getPlaylistItems();
 
-for (let i = 0; i < url.length; i++){
-(async () => {
+const limit = pLimit(5);
+ 
+const download = async (videoUrl, videoname) => {
   try {
-    const stream = await ytdl(url[i], {filter: "audioonly"});
-    stream.pipe(fs.createWriteStream(`${playlist[i]}.mp3`))
+
+    const stream = await ytdl(videoUrl, {filter: "audioonly"});
+    stream.pipe(fs.createWriteStream(`${videoname}.mp3`))
+
       .on("finish", () => {
         console.log("Download completed!");
       })
+
       .on("error", (error) => {
         console.error("Error writing to file:", error);
+
       });
   } catch (error) {
     console.error("Error downloading video:", error);
   }
-})();
+};
+
+const tasks = []; 
+
+for (let i = 0; i < url.length; i++) {
+  const videoUrl = url[i];
+  const fileName = playlist[i]; 
+
+  const task = limit(() => download(videoUrl, fileName));
+
+  tasks.push(task);
 }
+
+Promise.all(tasks).then(() => console.log("all downloads are complete"))
+
+
 
 
 
